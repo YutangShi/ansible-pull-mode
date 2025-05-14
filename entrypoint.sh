5ee95987da8556df682b635c9a56217d19977868
Allen Shi <allenshi@kkbox.com>
2025-05-14 18:13:20 +0800

    Add vault

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ mkdir -p /etc/crontab.d
 
 # 建立 crontab 檔案
 echo "# Ansible Pull 每小時執行" > /etc/crontab.d/ansible-cron
-echo "*/5 * * * * /usr/bin/ansible-pull -U https://github.com/YutangShi/ansible-pull-mode.git --vault-password-file /opt/docker/ansible-pull-mode/vault_password.txt >> /log/ansible-pull.log 2>&1" >> /etc/crontab.d/ansible-cron
+echo "*/5 * * * * /usr/bin/ansible-pull -U https://github.com/YutangShi/ansible-pull-mode.git  >> /var/log/ansible-pull.log 2>&1" >> /etc/crontab.d/ansible-cron
 echo "# Metrics 腳本每 5 分鐘執行" >> /etc/crontab.d/ansible-cron
 echo "*/5 * * * * /metrics.sh" >> /etc/crontab.d/ansible-cron
 
